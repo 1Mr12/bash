6ca3f543e5921c02f3df0403113945b0aa8402e3
mr12 <pubg.twitter12@gmail.com>
2021-10-30 15:44:39 +0200

    list option & comments

--- wifi/GetMacList.sh
+++ wifi/GetMacList.sh
@@ -1,15 +1,17 @@
 #!/bin/env bash
 
-packgeNeeded=("aircrack-ng" "iw" "wireless-tools" )
+packgeNeeded=("aircrack-ng" "iw" "wireless-tools" "network-manager" )
 
-
+# echo help if no arguments were given 
 if [ $# -eq 0 ]
 then
-    echo -e "-e for wifi essid\n-b for wifi bssid"
+    echo -e "-e for wifi essid\n-b for wifi bssid\n-h for help\n-l list wifi"
 fi
 
 # Get arguments 
-while getopts "b:e:r" option;
+# reset True > disable monitor mode befor exit
+# r and l without : coz thir is no input 
+while getopts "b:e:rl" option;
 do
     case $option in
         e) # set wiff essid
@@ -18,16 +20,22 @@ do
             wifiBssid=$OPTARG;;
         r)
             rest=True;;
+        h)
+            echo -e "-e for wifi essid\n-b for wifi bssid\n-r disable monitor mode\n-l list wifi"
+            ;;
+        l)
+            nmcli device wifi list 
+            ;;
         \?) # unexpected arguments 
             echo -e "\n-e for wifi essid\n-b for wifi bssid "
             exit;;
-
-
     esac
 done
 
 
-#dpkg -s $1 &> /dev/null [ $? -eq 0 ] > installed 1 > not installed
+# check for used packages
+# dpkg -s $1 &> /dev/null [ $? -eq 0 ]  0 > installed , 1 > not installed
+
 function installAllPackages(){
     read -p "Do you want to install missing Packages [y]: " answer
     if [[ $answer =~ [yY] ]];then sudo apt-get install -y ${packgeNeeded[@]}  ;else echo You must install all packges && exit ;fi
@@ -35,11 +43,14 @@ function installAllPackages(){
 }
 
 
-
+# start monitor mode 
 function startMonitorMode {
+    # get the current interface name
     interface=$(iw dev | grep Interface |cut -d " " -f2)
-    interfaceMode=$(iwconfig $interface |grep -o Monitor)
     
+    # Check if monitor mode is on 
+    interfaceMode=$(iwconfig $interface |grep -o Monitor)
+    # if $interfaceMode is not empty > return interface name else start monitor mode
     if [ ! -z $interfaceMode ]
     then
         echo "$interface"
@@ -52,11 +63,15 @@ function startMonitorMode {
 
 }
 
+# stor monitor mode 
 function stopMonitorMode() {
     sudo airmon-ng stop $1 > /dev/null 2>&1
     echo seting MonitorMode off
+    # Check if monitor mode is off
     interface=$(iw dev | grep Interface |cut -d " " -f2)
     interfaceMode=$(iwconfig $interface |grep -o Monitor)
+    # if monitor mode still enabled 
+    # Try another way to stop it 
     if [ ! -z $interfaceMode ]
     then
         sudo ifconfig $interface down 
@@ -69,32 +84,36 @@ function stopMonitorMode() {
 }
 
 
+# Get mac list 
 function GetMacList() {
+    # start monitor mode and return the name of the interface
     interfacee=$(startMonitorMode)
     echo $interfacee is on Monitor Mode
     sudo airodump-ng $1 -i $interfacee --output-format csv -w temp
     clear
+    # grep the devices mac
     cat temp-01.csv | cut -d , -f 1 
     sudo rm -f temp*
-}
-
-dpkg -s ${packgeNeeded[@]} > /dev/null 2>&1 || installAllPackages 
-
-if [ ! -z $wifiBssid ]
-then
-    GetMacList "-d $wifiBssid"
     if [ $rest ]
     then
         stopMonitorMode $interfacee
     fi
+}
+
+# Check for all needed packges first  - if one is missing > install all
+dpkg -s ${packgeNeeded[@]} > /dev/null 2>&1 || installAllPackages 
+
+
+# if the bessid is given 
+if [ ! -z $wifiBssid ]
+then
+    # Start GetMacList function with essid option
+    GetMacList "-d $wifiBssid"
     exit
 elif [ ! -z $wifiEssid ]
 then
+    # Start GetMacList function with essid option
     GetMacList "--essid $wifiEssid"
-    if [ $rest ]
-    then
-        stopMonitorMode $interfacee
-    fi
     exit
 else
     echo "You Must Give Essid or bssid "
