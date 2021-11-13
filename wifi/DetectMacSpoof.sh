ddf99abfd689c9f436c887857d3fe743bf20236a
mr12-home <pubg.twitter12@gmail.com>
2021-11-13 10:26:15 +0200

    add comments

--- wifi/DetectMacSpoof.sh
+++ wifi/DetectMacSpoof.sh
@@ -1,12 +1,7 @@
 #!/usr/bin/env bash
 
+# all the  pachages needed in the script
 packgeNeeded=("arp-scan")
-
-function installAllPackages(){
-    read -p "Do you want to install missing Packages [y]: " answer
-    if [[ $answer =~ [yY] ]];then sudo apt-get install -y ${packgeNeeded[@]} && exit ;else echo You must install all packges && exit ;fi
-    
-}
 
 
 # echo help if no arguments were given 
@@ -16,48 +11,18 @@ then
     exit
 fi
 
-<< "subnet"
-
-for SUBNET in {1..255}
-do
-for HOST in {1..255}
-do
-echo "[*] IP: "$PREFIX"."$SUBNET"."$HOST
-arping –c 3 –i $INTERFACE $PREFIX"."$SUBNET"."$HOST 2>
-/dev/null
-done
-done
+# set the interface name 
+INTERFACE=$1
 
 
+# ================================= Functions ================================= #
 
-function detectArpSpoof(){
-    for subnet in {1..255}
-    do
-        result=$(sudo arping -r -c 1 -d -i $INTERFACE $PREFIX.$subnet)
-        #echo "what " $result
-        if [ ! -z $result ]
-        then
-            NumberOfMac=$(echo $result | wc -l )
-            echo " $NumberOfMac For  $PREFIX.$subnet $result " 
-            if [[  ($NumberOfMac > 1) ]]
-            then
-                echo "Mac spoofing detected" echo -n "[*] Ip: $PREFIX.$subnet " $result
-            else
-                echo -n "[*] Ip: $PREFIX.$subnet " $result
-                echo ""
-            fi
-        else
-            echo -en " $subnet \r"
-        fi
-    done
+# check if all used packages are installed
+function installAllPackages(){
+    read -p "Do you want to install missing Packages [y]: " answer
+    if [[ $answer =~ [yY] ]];then sudo apt-get install -y ${packgeNeeded[@]} && exit ;else echo You must install all packges && exit ;fi
+    
 }
-
-
-subnet
-
-
-PREFIX=$1
-INTERFACE=$2
 
 
 function finish() {
@@ -66,11 +31,12 @@ function finish() {
 }
 
 
-# pass $interfaceName
+# pass interfaceName 
 function detectMacSpoof(){
+    # get list of all connected ip and mac
     listOfIpMac=$(sudo arp-scan -I $1 --localnet -q -x | cut -f 1 |sort)
     removeDuplication=$(echo -e "$listOfIpMac" | uniq )
-    if [[ "$listOfIpMac" == "$removeDuplication" ]]
+    if [[ "$listOfIpMac" == "$removeDuplication" ]] # No change after removing the duplicate
     then
         echo -e "No mac spoofing detected "
     else
@@ -81,19 +47,23 @@ function detectMacSpoof(){
     fi
 }
 
+# ================================= Functions ================================= #
 
 
-
+# Check for all needed packges first  - if one is missing > install all
 dpkg -s ${packgeNeeded[@]} > /dev/null 2>&1 || installAllPackages 
 
+# ================================= Main ================================= #
 
+# wait for Ctrl + c Signal to exit
 trap finish SIGINT
 
+# Check for sudo or root 
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root" 
    exit 1
 else
-    detectMacSpoof $1
+    detectMacSpoof $INTERFACE
 fi
 
 
