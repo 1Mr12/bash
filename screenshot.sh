5a698a99aeb4da0b01a892e3419cd24fa2d6f621
mr12-home <pubg.twitter12@gmail.com>
2021-11-13 10:37:33 +0200

    add comments

--- screenshot.sh
+++ screenshot.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# all the  pachages needed in the script
+packgeNeeded=("dmenu" "graphicsmagick-imagemagick-compat" "xclip" "espeak")
+
+
+# check if all used packages are installed
+# dpkg -s $1 &> /dev/null [ $? -eq 0 ]  0 > installed , 1 > not installed
+
+function installAllPackages(){
+    read -p "Do you want to install missing Packages [y]: " answer
+    if [[ $answer =~ [yY] ]];then sudo apt-get install -y ${packgeNeeded[@]}  ;else echo You must install all packges && exit ;fi
+    
+}
+
+
+# Check for all needed packges first  - if one is missing > install all
+dpkg -s ${packgeNeeded[@]} > /dev/null 2>&1 || installAllPackages 
+
+
 choice=$(echo -e "Copy\nCrop\nFull" | dmenu -i -p "Chosse")
 
 case $choice in
@@ -13,6 +31,6 @@ case $choice in
 	gnome-screenshot && espeak Full
     ;;
   *)
-	espeak "Error "
+	espeak "Error"
     ;;
 esac
